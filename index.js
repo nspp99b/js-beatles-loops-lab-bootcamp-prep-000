240778b121e4d144461c0a920fffabee2d9541a3
Shahin Motia <chromepanthers@gmail.com>
2017-10-11 02:26:56 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,9 @@
+//Beatles Play Challenge
+
+function theBeatlesPlay(musicians, instruments) {
+  var playaz = []
+  for (let i = 0; i < musicians.length; i++) {
+    playaz.push(`${musician[${i}]} plays ${instruments[${i}}`)
+  }
+  return playaz
+}
