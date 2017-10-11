515ef27c269d3ee508fc583b5e403959d8f83f42
Shahin Motia <chromepanthers@gmail.com>
2017-10-11 03:23:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,18 @@ function johnLennonFacts(array) {
   var faxx = new Array()
   let i = 0
   while (i < array.length) {
-    faxx.push(`${array[i++]}!!!`)
+    faxx.push(`${array[i++]}!!!`);
   }
   return faxx
+}
+
+//Beatles love
+
+function iLoveTheBeatles(n) {
+  var worship = new Array()
+  do {
+    worship.push("I love the Beatles");
+    n++
+  } while (n < 15)
+  return worship
 }
