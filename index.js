31963eb0dda7df994b3165d559e9fadb77d254c0
Shahin Motia <chromepanthers@gmail.com>
2017-10-11 02:37:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 function theBeatlesPlay(musicians, instruments) {
   var playaz = newArray()
   for (let i = 0; i < musicians.length; i++) {
-    playaz.push(`${musicians[${i}]} plays ${instruments[${i}}`);
+    playaz.push(`${musicians[i]} plays ${instruments[i]});
   }
   return playaz
 }
